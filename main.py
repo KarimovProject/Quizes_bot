b21126fbbde0626b3ae35d9a665e3809576b24d3
KarimovProject <abdullatifkarimov772@gmail.com>
2022-12-16 21:53:44 +0500

    fix

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
+from telegram import KeyboardButton,Update
 import telegram
+from telegram.ext import Updater, CommandHandler, CallbackContext
+import requests
+import json
 
 
 TOKEN = "5874141721:AAHprnHkQKf7XnaIGSinLI1Y-j8T2Blc_kI"
 
 bot = telegram.Bot(token=TOKEN)
 
-print(bot.get_me())
+class Quizes_bot:
+    def start(update:Update, context:CallbackContext):
+        txt = "Salom"
+        id = update.message.from_user.id
+        url = 'http://127.0.0.1:8000/api/quiz/'
+        rq = requests.get(url)
+        data_json = rq.json()
+        reply_markup = KeyboardButton[data_json['title']]
+        updater.bot.sendMessage(txt, id, 'Fanlar ro\'yxati', reply_markup=reply_markup)
+
+updater = Updater(TOKEN)
+
+updater.start_polling()
+updater.idle()
